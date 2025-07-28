bebf672b2547001ed48766873771cc6ae409d198
alasama25 <alasama351@gmail.com>
2025-07-28 10:36:24 +0000

    push

---
+++ 07-Memahami-String-Args/src/com/tutorial/Main.java
@@ -0,0 +1,8 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("halo " + args [0] + " jeleq");
+        System.out.println("halo " + args [1] + " puqi");
+    }
+}
